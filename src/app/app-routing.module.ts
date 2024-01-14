da29911ca49af9a7620db786135e91480603fb2e
Pedro Inácio <pedropedro_miguel@hotmail.com>
2024-01-14 03:23:11 +0000

    Url route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ElixirComponent } from './elixir/containers/elixir.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: '**', component: ElixirComponent} ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
